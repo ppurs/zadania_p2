134fbd2187f135b9e83c8e081e48bfa68bec7093
ppurs <aj@em.com>
2021-03-06 23:55:44 +0100

    test

--- tablica_nazwisk.cpp
+++ tablica_nazwisk.cpp
@@ -4,12 +4,16 @@ void nazwiska_string ( int n ) {
 
 }
 
+void nazwiska_C ( int n ) {
+
+}
 
 int main() {
 
     int n;
 
     nazwiska_string ( n );
-    
+    nazwiska_C ( n );
+
     return 0;
 }

9e43aa40b6001df736bf6b5b710a60f2688cd2fd
ppurs <paulinaaa523@gmail.com>
2021-03-07 19:27:21 +0100

    zad_1_cz_2

--- tablica_nazwisk_C.cpp
+++ tablica_nazwisk_C.cpp
@@ -1,4 +1,41 @@
-//
-// Created by Paulinaa on 2021-03-07.
-//
+#include <iostream>
 
+int main() {
+    int n;
+    int size = 0;
+
+    std::cin >> n;
+    char n_char = getchar();            // wczytuje znak nowej linii po 'n'
+
+    char** tab = new char* [ n ];
+
+    for ( int i = 0; i < n; i++ ) {
+        char letter;
+
+        letter = getchar();
+        char* temp = new char[ 50 ];
+
+        for ( size = 0; letter != '\r' && letter != '\n'; size++ ) {
+            temp[ size ] = letter;
+            letter = getchar();
+           }
+        temp[ size ] = '\0';
+
+        tab[ i ] = new char[ size + 1 ];       //bo tablica ma mieć wymiar: liczba znaków + 1
+        int k = 0;
+        for ( k = 0; k < size + 1 ; k++) {
+            tab[ i ][ k ] = temp [ k ];
+        }
+
+        delete[] temp;
+    }
+
+    for ( int j = 0; j < n; j++ ) {
+        delete[] tab[ j ];
+        tab[ j ] = NULL;
+        }
+
+    delete[] tab;
+
+    return 0;
+}

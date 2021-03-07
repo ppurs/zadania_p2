045a88c39772c25bce20f4b9e817359d0cb39ac8
ppurs <paulinaaa523@gmail.com>
2021-03-07 14:12:22 +0100

    add smart pointer

--- struktury_z2.cpp
+++ struktury_z2.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <memory>
 
 struct Stypendium {
     std::string rodzaj = "naukowe";
@@ -9,17 +10,24 @@ struct Student {
     std::string imie = "Jan";
     std::string nazwisko = "Kowalski";
     std::string rok_studiow = "2";
-    Stypendium* stypendium;
+    //Stypendium* stypendium;                   //klasycznie
+    std::shared_ptr<Stypendium>stypendium;
 };
 
 int main () {
 
     Student* student_1 = new Student;
 
-    Stypendium* s_1 = new Stypendium;
+    //Stypendium* s_1 = new Stypendium;                             // klasycznie
+    std::shared_ptr<Stypendium>s_1(new Stypendium);              // sprytny
     s_1->rodzaj = "socjalne";
     s_1->wysokosc = 750;
     student_1->stypendium = s_1;
+
+
+/*  delete student_1;                 // do klasycznej wersji
+    delete s_1;
+                */
 
     return 0;
 }
